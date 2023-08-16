1b9c0c8163401256f5634bdd2ba6d4d0fd3e0139
Vincent Son <107471539+szw0407@users.noreply.github.com>
2023-08-16 21:10:38 +0800

    Temporarily done

--- 1.js
+++ 1.js
@@ -10,15 +10,43 @@ function encodeHTML(source) {
 
 function showPopup() {
     // hljs.highlightElement(document.getElementById("code"))
-  var code = document.getElementById("code").value;
+  let code = document.getElementById("code").value;
+  //if code begins with <!DOCTYPE html>, remove it
+  if (code.substring(0, 15) == "<!DOCTYPE html>") {
+    code = code.substring(16);
+  }
+  //if code begins without <html> or <head>, do something
   
-    var popup = window.open("", "popup", "width=800,height=600");
-    popup.document.write("<!DOCTYPE HTML><html><head><title>你写的网页</title>");
-    popup.document.write("<link rel=\"stylesheet\" href=\"//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/styles/default.min.css\">");
-    popup.document.write("</head><body>");
-    popup.document.write(code);
-    popup.document.write("</body></html>");
+  var popup = window.open("", "popup", "width=800,height=600");
+  // if code begins with neither <html> nor <head>, add them:
+  if (code.substring(0, 6) != "<html>") {
 
+    var code2 = "<!DOCTYPE HTML><html>";
+    if (code.substring(0, 6) != "<head>") {
+      code2 += "<head>";
+      code2 += "<title>你写的网页</title>";
+      code2 += "</head>";
+    }
+    
+    if (code.substring(0, 6) != "<body>") {
+      code2 += "<body>";
+    }
+    code2 += code;
+    code2 += "</body></html>";
+  } else {
+    var code2 = "<!DOCTYPE HTML>"+code;
+  }
+    // popup.document.write("<!DOCTYPE HTML><html><head><title>你写的网页</title>");
+    // popup.document.write("<link rel=\"stylesheet\" href=\"//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/styles/default.min.css\">");
+    // popup.document.write("</head><body>");
+    // popup.document.write(code);
+    // popup.document.write("</body></html>");
+  popup.document.write(code2);
+  // make the main body codeout part show the highlighted code
+  document.getElementById("codeout").innerHTML = hljs.highlight(code, { language: 'html' }).value;
+  hljs.highlightAll();
+  // popup.document.close();
+  // render();
 }
 
 
@@ -28,9 +56,7 @@ function Button() {
   hljs.highlightAll();
   showPopup();
 
-  document.getElementById("code").style("visibility:hidden")
-  // render();
+  document.getElementById("code").remove();
+
 };
 
-function reset() {
-}
